a5153da23e48c3205daa27152e8d4156b8a5f474
shanemcg992 <shane.mcgarry@fathomtech.io>
2023-12-17 21:42:39 +0000

    pushing code up

--- CV_Shane/script.js
+++ CV_Shane/script.js
@@ -5,18 +5,6 @@ document.addEventListener('DOMContentLoaded', function() {
         this.classList.toggle('expanded');
     });
 
-    // Smooth scroll for links with hashes
-    $('a.nav-link').click(function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
 
     // Arrow toggle for collapsible elements
     $('.collapse').on('show.bs.collapse', function() {
@@ -24,28 +12,19 @@ document.addEventListener('DOMContentLoaded', function() {
     }).on('hide.bs.collapse', function() {
         $(this).prev().find('.fa-chevron-up').removeClass('fa-chevron-up').addClass('fa-chevron-down');
     });
-
-    // // Theme toggle script
-    // const themeToggleBtn = document.getElementById('themeToggle');
-    // themeToggleBtn.onclick = function() {
-    //     document.body.classList.toggle('dark-theme');
-    //     themeToggleBtn.innerText = document.body.classList.contains('dark-theme') ? 'Light Mode' : 'Dark Mode';
-    // };
 });
 
-// Scroll to top button functionality
-var mybutton = document.getElementById("myBtn");
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
+// Smooth scrolling for nav links
+document.querySelectorAll('.smooth-scroll').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; 
-}
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        if (targetSection) {
+            targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
+});
